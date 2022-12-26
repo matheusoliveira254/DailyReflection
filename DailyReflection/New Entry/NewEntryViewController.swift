5b324e6272cc9cbaf83ffad9ac06c41b2ac58490
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-12-26 16:19:07 -0700

    wip

---
+++ DailyReflection/New Entry/NewEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewEntryViewController.swift
+//  DailyReflection
+//
+//  Created by Matheus Oliveira on 12/26/22.
+//
+
+import UIKit
+
+class NewEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DailyReflection/New Entry/NewEntryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewEntryViewModel.swift
+//  DailyReflection
+//
+//  Created by Matheus Oliveira on 12/26/22.
+//
+
+import Foundation
